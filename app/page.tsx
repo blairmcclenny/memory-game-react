9398ba7f83615992936eb6a51c1b39a824f04cce
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-03-13 11:19:31 -0700

    refactor generate board

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-import { generateBoard } from "./board"
+"use client"
+
+import { useEffect, useState } from "react"
+import { generateBoard, Tile } from "./board"
 
 export default function Home() {
-  const board = generateBoard()
-  console.table(board)
+  const [board, setBoard] = useState<Tile[] | []>([])
 
-  return <div>Hello</div>
+  useEffect(() => {
+    setBoard(generateBoard(12))
+  }, [])
+
+  return (
+    <div className="container px-4 mx-auto">
+      <div className="grid grid-cols-4 p-8 bg-gray-100 gap-2">
+        {board.map((tile) => (
+          <div className="bg-gray-800 text-white p-2" key={tile.id}>
+            {tile.index}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
