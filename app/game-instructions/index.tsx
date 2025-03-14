e089abc8a5146b70c512c45abd4eeb61ca9416be
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-03-13 17:22:39 -0700

    move components and add game instructions copy

---
+++ app/game-instructions/index.tsx
@@ -0,0 +1,39 @@
+export default function GameInstructions() {
+  return (
+    <div>
+      <h2>Game Instructions</h2>
+      <div>A Game for 2 Players</div>
+      <h3>Objective</h3>
+      <p>Find more matches than the other player.</p>
+      <h3>Set-Up</h3>
+      <p>
+        Begin with all the doors on the board closed. Without looking at the
+        pictures, select one of the seven themed game cards and insert it into
+        the slot at the top of the board. Deicide which player will use the red
+        score tracker and which player will use the blue.
+      </p>
+      <h3>Basic Play</h3>
+      <ul>
+        <li>
+          The younger player goes first. Players take turns opening two doors,
+          one at a time.
+        </li>
+        <li>
+          If the pictures beind the doors match, the two doors remain open and
+          the player scores a point on his or her score tracker. Thie player
+          continures playing until failing to make a match.
+        </li>
+        <li>
+          When the picures behind two selected doors do not match, the doors are
+          shut and it becomes the next player&apos;s turn.
+        </li>
+        <h3>Winning the Game</h3>
+        <p>
+          The player with the most points after all of the matches have been
+          made is the winner! If the payers finish with the same number of
+          points, the game is a draw.
+        </p>
+      </ul>
+    </div>
+  )
+}
--- app/memory-game/index.tsx
+++ app/memory-game/index.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react"
 import { generateBoard, Tile } from "@/app/memory-game/board"
+import Door from "./Tile"
 
 export default function MemoryGame() {
   const [board, setBoard] = useState<Tile[] | []>([])
@@ -12,10 +13,13 @@ export default function MemoryGame() {
 
   return (
     <div className="grid grid-cols-4 p-8 bg-gray-100 rounded-lg gap-2">
-      {board.map((tile) => (
-        <div className="bg-gray-800 text-white p-2 rounded-sm" key={tile.id}>
-          {tile.index}
-        </div>
+      {board.map((door) => (
+        <Door
+          key={door.id}
+          value={door.value}
+          isOpen={door.isOpen}
+          hasBeenFound={door.hasBeenFound}
+        />
       ))}
     </div>
   )
