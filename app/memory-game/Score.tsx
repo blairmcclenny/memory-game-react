b296c53c7e95c64bbac78744188f5e6ab90b7c63
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-03-15 00:12:20 -0700

    styling

--- app/memory-game/Score.tsx
+++ app/memory-game/Score.tsx
@@ -1,12 +1,19 @@
+function Player({ position }: { position: number }) {
+  return (
+    <div className={`${position === 1 ? "text-red-500" : "text-blue-500"}`}>
+      PLAYER {position === 1 ? "1" : "2"}{" "}
+      <span className="font-mono text-white bg-stone-700 p-2 rounded-md min-w-[4ch] inline-block text-center">
+        0
+      </span>
+    </div>
+  )
+}
+
 export default function Score() {
   return (
     <div className="font-serif flex justify-center gap-8 text-2xl">
-      <div className="text-red-500">
-        PLAYER 1 <span>00</span>
-      </div>
-      <div className="text-blue-500">
-        PLAYER 2 <span>00</span>
-      </div>
+      <Player position={1} />
+      <Player position={2} />
     </div>
   )
 }
