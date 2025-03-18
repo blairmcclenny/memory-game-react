588d97f01df3f2e36760312c7c464206e983491b
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-03-17 23:58:15 -0700

    basic results screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${ericaOne.variable} container px-4 mx-auto antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${ericaOne.variable} container px-4 pb-16 mx-auto antialiased`}
       >
         <header className="mb-16 mx-auto text-center">
           <button className="font-bold text-xs rounded-md rounded-tr-none rounded-tl-none bg-yellow-300 py-2 px-4 tracking-wide inline-flex gap-1 items-center cursor-pointer">
