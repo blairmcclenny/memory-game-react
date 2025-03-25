d8b1904de762b4138d4e056246f23ac917ee8ffc
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-03-25 03:05:29 -0700

    instructions wip

--- app/game-instructions/index.tsx
+++ app/game-instructions/index.tsx
@@ -1,39 +1,66 @@
+import { X } from "lucide-react"
+import RotatedWord from "../RotatedWord"
+import { ReactNode } from "react"
+
+function H3({ children }: { children: ReactNode }) {
+  return (
+    <h3 className="font-serif text-2xl sm:text-4xl mt-8 mb-3">{children}</h3>
+  )
+}
+
 export default function GameInstructions() {
   return (
-    <div>
-      <h2>Game Instructions</h2>
-      <div>A Game for 2 Players</div>
-      <h3>Objective</h3>
-      <p>Find more matches than the other player.</p>
-      <h3>Set-Up</h3>
-      <p>
-        Begin with all the doors on the board closed. Without looking at the
-        pictures, select one of the seven themed game cards and insert it into
-        the slot at the top of the board. Deicide which player will use the red
-        score tracker and which player will use the blue.
-      </p>
-      <h3>Basic Play</h3>
-      <ul>
-        <li>
-          The younger player goes first. Players take turns opening two doors,
-          one at a time.
-        </li>
-        <li>
-          If the pictures beind the doors match, the two doors remain open and
-          the player scores a point on his or her score tracker. Thie player
-          continures playing until failing to make a match.
-        </li>
-        <li>
-          When the picures behind two selected doors do not match, the doors are
-          shut and it becomes the next player&apos;s turn.
-        </li>
-        <h3>Winning the Game</h3>
+    <div className="absolute inset-0 min-h-screen min-w-screen bg-stone-100 text-center">
+      <button
+        // onClick={() => setShowModal(true)}
+        className="font-bold font-sans text-xs border-2 border-t-0 rounded-md rounded-tr-none rounded-tl-none bg-yellow-300 py-2 px-4 tracking-wide inline-flex gap-1 items-center cursor-pointer"
+      >
+        <X size={20} /> CLOSE
+      </button>
+      <div className="p-4 pt-0 max-w-2xl mx-auto font-mono">
+        <h2 className="font-serif text-4xl sm:text-6xl mt-4">
+          <RotatedWord word={"INSTRUCTIONS"} />
+        </h2>
+        <div className="text-2xl mb-8">A Game for 2 Players</div>
+        <H3>
+          <RotatedWord word={"OBJECTIVE"} />
+        </H3>
+        <p>Find more matches than the other player.</p>
+        <H3>
+          <RotatedWord word={"SET-UP"} />
+        </H3>
+        <p>
+          Begin with all the doors on the board closed. Deicide which player
+          will use the red score tracker and which player will use the blue.
+        </p>
+        <H3>
+          <RotatedWord word={"BASIC"} /> <RotatedWord word={"PLAY"} />
+        </H3>
+        <ul className="list-disc list-inside space-y-3">
+          <li>
+            The younger player goes first. Players take turns opening two doors,
+            one at a time.
+          </li>
+          <li>
+            If the pictures beind the doors match, the two doors remain open and
+            the player scores a point on his or her score tracker. The player
+            continures playing until failing to make a match.
+          </li>
+          <li>
+            When the picures behind two selected doors do not match, the doors
+            are shut and it becomes the next player&apos;s turn.
+          </li>
+        </ul>
+        <H3>
+          <RotatedWord word={"WINNING"} /> <RotatedWord word={"THE"} />{" "}
+          <RotatedWord word={"GAME"} />
+        </H3>
         <p>
           The player with the most points after all of the matches have been
           made is the winner! If the players finish with the same number of
           points, the game is a draw.
         </p>
-      </ul>
+      </div>
     </div>
   )
 }
